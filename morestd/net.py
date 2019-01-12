ac653f853852846b82d97371d48fb44152264fc3
Ferry Boender <ferry.boender@gmail.com>
2019-01-12 15:55:50 +0100

    moreutils.net: Added fqdn() to get fully qualified domainname

---
+++ morestd/net.py
@@ -0,0 +1,21 @@
+#
+# morestd: More standard libraries for Python
+#
+# Copyright 2010-2018 Ferry Boender, released under the MIT license
+#
+
+import socket
+
+
+def fqdn():
+    """
+    Return Fully qualified domain name for this machine.
+
+    Many machines have their fully qualified domain name (fqdn) incorrectly
+    configured. For example, the hostname may contain the fqdn while the
+    domainname part is empty. This function attempts to return a correct FQDN.
+    """
+    fqdn = socket.gethostname()
+    if '.' not in fqdn:
+        fqdn = socket.getfqdn()
+    return fqdn
