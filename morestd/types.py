cafc56a11d92db46e3945094da35393b74137694
Ferry Boender <ferry.boender@gmail.com>
2019-01-12 15:52:10 +0100

    morestd.types: Added to_bool(): convert string to bool.

---
+++ morestd/types.py
@@ -0,0 +1,20 @@
+#
+# morestd: More standard libraries for Python
+#
+# Copyright 2010-2018 Ferry Boender, released under the MIT license
+#
+
+def to_bool(s):
+    """
+    Convert string `s` into a boolean.
+
+    `s` can be 'true', 'True', 1, 'false', 'False', 0.
+    """
+    if isinstance(s, bool):
+        return s
+    elif s.lower() in ['true', '1']:
+        return True
+    elif s.lower() in ['false', '0']:
+        return False
+    else:
+        raise ValueError("Can't cast '%s' to bool" % (s))
