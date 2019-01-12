b91d6f0791ad338425666bec3e445bee09c9fea9
Ferry Boender <ferry.boender@gmail.com>
2019-01-12 12:31:12 +0100

    Initial structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# MoreSTD: More standard libraries for Python
+
+A collection of additional standard libraries for Python.
+
+
---
+++ morestd/__init__.py
@@ -0,0 +1,7 @@
+#
+# morestd: More standard libraries for Python
+#
+# Copyright 2010-2018 Ferry Boender, released under the MIT license
+#
+
+version = (0, 1)
