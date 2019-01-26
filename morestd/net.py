335d5157ab91d3cd2a3afbc760f9bdd609c2fead
Ferry Boender <ferry.boender@gmail.com>
2019-01-26 08:59:04 +0100

    net.hex_ip4: Convert a hex IPv4 as found in /proc/net/tcp to a dotted IP

--- morestd/net.py
+++ morestd/net.py
@@ -21,6 +21,21 @@ def fqdn():
     return fqdn
 
 
+def hex_ip4(ip):
+    """
+    Convert a hex IPv4 as found in /proc/net/tcp to a dotted IP.
+
+    >>> hex_ip4("0E01A8C0")
+    '192.168.1.14'
+    """
+    return(".".join([
+        str(int(ip[6:8], 16)),
+        str(int(ip[4:6], 16)),
+        str(int(ip[2:4], 16)),
+        str(int(ip[0:2], 16)),
+    ]))
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
